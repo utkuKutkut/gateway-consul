87e599584e5491ccd67dbb783022afe84f549c66
utkuKutkut <uckutkut@dons.usfca.edu>
2022-04-03 17:31:12 -0700

    update

--- service1/src/main/groovy/com/example/Service2Client.groovy
+++ service1/src/main/groovy/com/example/Service2Client.groovy
@@ -3,8 +3,11 @@ package com.example
 import com.example.domain.Book
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.retry.annotation.CircuitBreaker
+import io.micronaut.retry.annotation.Retryable
 import io.reactivex.Flowable
-
+@Retryable(attempts = "10", delay = "2s")
+@CircuitBreaker(delay = "5s", attempts = "5", multiplier = "3", reset = "300s")
 @Client(id = "service2")
 interface Service2Client {
     @Get("/books")
